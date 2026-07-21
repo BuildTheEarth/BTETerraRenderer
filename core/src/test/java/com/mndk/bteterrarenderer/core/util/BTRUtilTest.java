95150f824a9d65dd233b37496f84d0e0b4224b13
Zoriot <zoriot@proton.me>
2026-07-21 19:51:31 +0200

    fix: Update double formatting tests to use locale-specific decimal separator
    So it's not independent of the running plattform/locale.

--- core/src/test/java/com/mndk/bteterrarenderer/core/util/BTRUtilTest.java
+++ core/src/test/java/com/mndk/bteterrarenderer/core/util/BTRUtilTest.java
@@ -4,12 +4,15 @@ import com.mndk.bteterrarenderer.util.StringUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class BTRUtilTest {
 
     @Test
     public void testDoubleFormatter() {
-        Assert.assertEquals("0.250", StringUtil.formatDoubleNicely(0.25, 3));
-        Assert.assertEquals("0.500", StringUtil.formatDoubleNicely(0.5, 3));
+        Assert.assertEquals("0" + new DecimalFormatSymbols(Locale.getDefault()).getDecimalSeparator() + "250", StringUtil.formatDoubleNicely(0.25, 3));
+        Assert.assertEquals("0" + new DecimalFormatSymbols(Locale.getDefault()).getDecimalSeparator() + "500", StringUtil.formatDoubleNicely(0.5, 3));
         Assert.assertEquals("1", StringUtil.formatDoubleNicely(1, 3));
     }
 
